c78e7001b1c30be67b5ccb57dc267f3a81d9eefe
Fbullman <88170636+Fbullman@users.noreply.github.com>
2021-10-16 08:27:54 -0700

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,66 +1,49 @@
---Retirement employees in Sales and Development department
 
---PART 1
 
-SELECT e.emp_no,
-e.first_name,
-e.last_name,
-ti.title,
-ti.from_date,
-ti.to_date
+--Retirement employees in Sales and Development dept.
+SELECT de.emp_no,
+    de.first_name,
+    de.last_name,
+    ti.title,
+	ti.from_date,
+    ti.to_date
 INTO retirement_titles
-FROM employees as e
-INNER JOIN titles AS ti
-ON (e.emp_no = ti.emp_no)
+FROM employees AS e
+LEFT JOIN titles AS ti
+ON de.emp_no=  ti.emp_no
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows - part 2/Deliverable 1
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
 INTO unique_titles
-FROM retirement_titles
-ORDER BY emp_no, to_date DESC;
+FROM ret_titles
+ORDER BY emp_no ASC, to_date DESC;
 
---part 3 count retiring employees by recent titles 
-SELECT DISTINCT ON (title) title
-FROM unique_titles
-ORDER BY title
 
-SELECT COUNT(title) AS "Count", title AS "Title"
+-- Counting the number of each employee per title
+SELECT COUNT(title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
-ORDER BY "Count" desc;
+ORDER BY count DESC;
 
---Deliverable part 2 Mentorship
-
-SELECT DISTINCT ON (emp_no)
-e.emp_no, 
-e.first_name, 
-e.last_name, 
-e.birth_date,
-de.from_date,
-de.to_date,
-ti.title
-INTO mentorship_eligibilty
-from employees as e
+-- Creating a list of employees eligible for potential mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+  e.first_name,
+  e.last_name,
+  e.birth_date,
+  de.from_date,
+  de.to_date,
+  ti.title
+INTO mentorship
+FROM employees as e
 INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
+  ON (e.emp_no = de.emp_no)
 INNER JOIN titles as ti
-ON (e.emp_no = ti.emp_no)
+  ON (e.emp_no = ti.emp_no)
 WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY emp_no;
-
-select * from mentorship_eligibilty
-limit 10
-
-select title, count(*) as "Mentorship_Eligibility_by_Title"
-INTO mentorship_eligibilty_count_by_title
-from mentorship_eligibilty
-GROUP BY title
-
-select * from mentorship_eligibilty_count_by_title;
+ORDER BY e.emp_no;
